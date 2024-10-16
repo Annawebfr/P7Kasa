e144e19db831ff45070e7d1beee67a601fbf861d
Anna Hultgren <annahultgren5@gmail.com>
2024-10-16 15:23:30 +0200

    code chg

---
+++ kasa/src/data/data.js
@@ -0,0 +1,40 @@
+$primary : #fff;
+$secondary : #FF6060;
+$tertiary : #F7F7F7;
+
+*,
+::before,
+::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat';
+    font-style: normal;
+}
+
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
+li {
+    list-style: none;
+}
+
+body {
+    max-width: 1440px;
+    margin: 0 auto;
+}
+
+.content-wrap {
+    padding: 45px;
+
+    @media screen and (max-width: 1440px) {
+        padding: 25px;
+        margin-top: 10px;
+    }
+
+    @media screen and (max-width: 425px) {
+        padding: 20px;
+    }
+}
